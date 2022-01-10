b1e9c26c202793179f03e48097fb664c222d9c8a
mohammad-abbas-me <mohammadabbas.me@tigeranalytics.com>
2022-01-10 16:26:12 +0530

    Added kubernetes pod operator in dag  and CeleryKubernetesExecutor in values.yaml

--- dags/docker_airflow.py
+++ dags/docker_airflow.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
@@ -18,7 +19,21 @@ default_args = {
 
 dag = DAG("docker_sample", default_args=default_args, schedule_interval=None)
 
-start = DummyOperator(task_id="run_this_first", dag=dag)
+start = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
-passing = BashOperator(task_id="print_date", bash_command="date", dag=dag)
+passing = KubernetesPodOperator(
+    namespace="default",
+    image="vdinesh1990/test_airflow:v1",
+    cmds=None,
+    arguments=["52"],
+    # volume_mounts=volume_mount_list,
+    # volumes=volume_list,
+    labels={"foo": "bar"},
+    name="passing-test",
+    task_id="passing-task",
+    in_cluster=True,
+    # config_file="/home/dinesh.velmuruga/.kube/config",
+    get_logs=True,
+    dag=dag,
+)
 passing.set_upstream(start)
